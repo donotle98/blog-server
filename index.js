8207d98ef2c9a6b5d795f3b6f8b7155a63540623
Donovan Le <dtle647@gmail.com>
2021-02-22 13:34:21 -0600

    initial commit

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ require('dotenv').config();
 
 //connect to mongodb
 const dbURI = `mongodb+srv://donole:${DB_PW}@cluster0.2ke1l.mongodb.net/blog-site?retryWrites=true&w=majority`;
-mongoose
-    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) =>
-        app.listen(process.env.PORT || 5000, () =>
-            console.log(
-                `Connected to MongoDB, Express is running on port ${
-                    process.env.PORT || 5000
-                }!`
-            )
+
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+client.connect((err) => {
+    const collection = client.db('test').collection('devices');
+    // perform actions on the collection object
+    app.listen(process.env.PORT || 5000, () =>
+        console.log(
+            `Connected to MongoDB, Express is running on port ${
+                process.env.PORT || 5000
+            }!`
         )
-    )
-    .catch((e) => console.log(e));
+    );
+    client.close();
+});
